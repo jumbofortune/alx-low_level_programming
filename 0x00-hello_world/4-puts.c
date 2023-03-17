fc1767c51b2d467581f9b61e1500b573c0c06d09
jumbofortune <ibijumbo06@gmail.com>
2023-03-16 17:29:33 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - puts to print “Programming is like building
+ * main - puts uning puts to print “Programming is like building
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts(“\”Programming is like building a multilingual puzzle”);
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
