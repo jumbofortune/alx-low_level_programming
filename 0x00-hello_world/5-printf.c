48a38742318f81204b8362ecc8ba5050db1e9f7e
jumbofortune <ibijumbo06@gmail.com>
2023-03-16 17:41:56 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - puts uning puts to print “Programming is like building
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
